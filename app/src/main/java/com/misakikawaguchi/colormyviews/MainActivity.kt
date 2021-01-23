7ff1abe7a5d98cded56ac895201a209b7566ee34
川口美咲 <misaki@arcphilia.co.jp>
2021-01-23 16:23:00 +0900

    [Add]setListeners追加

--- app/src/main/java/com/misakikawaguchi/colormyviews/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/colormyviews/MainActivity.kt
@@ -4,14 +4,32 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setListeners()
     }
 
+    private fun setListeners() {
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableViews: List<View> = listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, rootConstraintLayout)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
 
     // 色を変える
     private fun makeColored(view: View) {
